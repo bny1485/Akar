5104f5f0d373b573f6a47b273be9d71ad16d8492
bny1485 <benyaminmahmoudyan@gmail.com>
2020-10-17 00:48:33 +0330

    change in post-detail

--- Post/views.py
+++ Post/views.py
@@ -5,7 +5,6 @@ from django.views.generic import ListView, DetailView
 
 class Album(ListView):
     """This function return post list viwe which user can see all post in one or several page"""
-    model = Post
     queryset = Post.objects.all()
     template_name = 'Album.html'
 
@@ -16,6 +15,7 @@ class Post_detail(DetailView):
     template_name = "post_detail.html"
 
     def get_context_data(self, *args, objects_list=None, **kwargs):
-        context = super(Post_detail, self).get_context_data(
-            *args, **kwargs)
+        context = super(Post_detail, self).get_context_data(*args, **kwargs)
+        context['abc'] = "my name is Benyamin"
+        print(context   )
         return context
